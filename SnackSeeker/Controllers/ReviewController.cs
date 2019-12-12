c57a4d11389efe2e346829825886517a9088cf58
Andrew Wilder <wilderandrew9@gmail.com>
2019-12-12 10:43:38 -0500

    Add and Update Reviews

---
+++ SnackSeeker/Controllers/ReviewController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SnackSeeker.Models;
+
+namespace SnackSeeker.Controllers
+{
+	public class ReviewController : Controller
+	{
+
+		private readonly string _yelpKey;
+		private readonly HttpClient _client;
+		private readonly SnacksDbContext _context;
+
+
+		public ReviewController(IConfiguration configuration, SnacksDbContext context)
+		{
+			_yelpKey = configuration.GetSection("ApiKeys")["YelpApi"];
+			_client = new HttpClient();
+			_client.BaseAddress = new Uri("https://api.yelp.com/v3/");
+			_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _yelpKey);
+			_context = context;
+		}
+		public async Task<IActionResult> ReviewCheck(string id)
+		{
+			string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+			var reviews = _context.Review.Where(x => x.UserId == id).ToList();
+			foreach (var review in reviews)
+			{
+				if(review.RestaurantId == id)
+				{
+					return View("ReviewChange", review);
+				}
+			}
+			
+			var reviewResponse = await _client.GetAsync($"businesses/{id}");
+			var results = await reviewResponse.Content.ReadAsAsync<RestaurantRoot>();
+
+			return View("ReviewAdd" );
+		}
+		
+
+	}
+	
+}
+		
---
+++ SnackSeeker/Models/YelpId.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnackSeeker.Models
+{
+	public class Open
+	{
+		public bool is_overnight { get; set; }
+		public string start { get; set; }
+		public string end { get; set; }
+		public int day { get; set; }
+	}
+	public class Hour
+	{
+		public List<Open> open { get; set; }
+		public string hours_type { get; set; }
+		public bool is_open_now { get; set; }
+	}
+	public class SpecialHour
+	{
+		public string date { get; set; }
+		public object is_closed { get; set; }
+		public string start { get; set; }
+		public string end { get; set; }
+		public bool is_overnight { get; set; }
+	}
+	public class RestaurantRoot
+	{
+		public string id { get; set; }
+		public string alias { get; set; }
+		public string name { get; set; }
+		public string image_url { get; set; }
+		public bool is_claimed { get; set; }
+		public bool is_closed { get; set; }
+		public string url { get; set; }
+		public string phone { get; set; }
+		public string display_phone { get; set; }
+		public int review_count { get; set; }
+		public List<Category> categories { get; set; }
+		public double rating { get; set; }
+		public Location location { get; set; }
+		public Coordinates coordinates { get; set; }
+		public List<string> photos { get; set; }
+		public string price { get; set; }
+		public List<Hour> hours { get; set; }
+		public List<object> transactions { get; set; }
+		public List<SpecialHour> special_hours { get; set; }
+	}
+}
+
