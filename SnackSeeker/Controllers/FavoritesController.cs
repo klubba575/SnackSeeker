8c92cfe2e21007ac76c70b162f303872d24f2874
tcotter1432 <tcotter1432@gmail.com>
2019-12-16 14:18:40 -0500

    FavoritesController Comments

--- SnackSeeker/Controllers/FavoritesController.cs
+++ SnackSeeker/Controllers/FavoritesController.cs
@@ -15,10 +15,12 @@ namespace SnackSeeker.Controllers
     [Authorize]
     public class FavoritesController : Controller
     {
+        //Dependency Injection variable Instantiation
         private readonly string _yelpKey;
         private readonly HttpClient _client;
         private readonly SnacksDbContext _context;
 
+        //Constructor for Dependency Injection
         public FavoritesController(IConfiguration configuration, SnacksDbContext context)
         {
             _yelpKey = configuration.GetSection("ApiKeys")["YelpApi"];
@@ -28,15 +30,22 @@ namespace SnackSeeker.Controllers
             _context = context;
         }
 
+        //Displays the Logged-in user's favorites
         public IActionResult DisplayFavorites()
         {
+            //gets the user ID for the currently logged in user
             string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            //Pull the favorites from the FavoriteList Table and then sort the list, taking only those that are owned by the currently logged in user
             var favorites = _context.FavoritesList.Where(x => x.UserId == id).ToList();
-
+            //return the favorites to the view
             return View(favorites);
         }
+
+        //Method to add a favorite to the logged-in user's list
+        //Id is the restaurant's Id in the Yelp API, string name is the restaurant to be added and string price is the Yelp price rating
         public IActionResult AddToFavorites(string Id, string name, string price)
         {
+            //Get the currently logged-in user userID
             string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
             var reviews = _context.Review.Where(x => x.UserId == id).ToList();
 
